550f470f8bde66b6e7e3805d7a9bbb6a6a9dbe01
Josegit <N00034975@upn.pe>
2025-05-07 17:34:27 -0500

    Sem07

--- sem07/Program.cs
+++ sem07/Program.cs
@@ -11,10 +11,7 @@ namespace sem07
     {
         static void Main(string[] args)
         {
-            NumeroPerfecto();
-            NumeroPerfecto();
-            NumeroPerfecto();
-            NumeroPerfecto();
+            NumeroPerfectoDoWhile();
         }
             //engloba codigo (llaves). Static: modulador de acceso (quien puede acceder o no).
             //Void; retorno (vacio). Main: nombre.(string[] args): parametro
@@ -40,6 +37,49 @@ namespace sem07
                     Console.WriteLine("el numero no es perfecto");
                 }
             }
-        
+        static void NumeroPerfectoWhile()
+        {
+            int num, suma = 0, div;
+            Console.WriteLine("ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            div = num-1;
+
+            while (div>0)
+            {
+                if (num%div==0)
+                {
+                    suma = suma + div;
+                }
+                div--;
+            }
+            Console.WriteLine("suma: " + suma);
+        }
+        static void NumeroPerfectoDoWhile()
+        {
+            int num, suma = 0, div;
+            Console.WriteLine("ingrese un numero: ");
+            num = int.Parse(Console.ReadLine());
+
+            div = num - 1;
+            do
+            {
+
+                if (num % div == 0)
+                {
+                    suma = suma + div;
+                }
+                div--;
+            }
+            while (div>0);
+            if (suma == num)
+            {
+                Console.WriteLine("El numero es perfecto");
+            }
+            else
+            {
+                Console.WriteLine("el numero no es perfecto");
+            }
+        }
     }
 }
